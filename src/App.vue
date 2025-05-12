fc201017f98ed0398c076ccf43dbfdf49a5d7a66
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:27:54 +0300

    task 69

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,35 @@
 <template>
-  <div>
-    <Employee
-      v-for="user in users"
-      :key="user.id"
-      :id="user.id"
-      :name="user.name"
-      :surn="user.surn"
-      @remove="remove"
-    />
-  </div>
+	<div>
+		<Employee v-for="user in users" :key="user.id" :id="user.id" :name="user.name" :surn="user.surn" @change="change" />
+	</div>
 </template>
 
 <script>
 import Employee from './components/Employee.vue';
 
 export default {
-  components: {
-    Employee,
-  },
-  data() {
-    return {
-      users: [
-        { id: 1, name: 'name1', surn: 'surn1' },
-        { id: 2, name: 'name2', surn: 'surn2' },
-        { id: 3, name: 'name3', surn: 'surn3' },
-      ],
-    };
-  },
-  methods: {
-    remove(id) {
-      this.users = this.users.filter((user) => user.id !== id);
-    },
-  },
+	components: {
+		Employee,
+	},
+	data() {
+		return {
+			users: [
+				{ id: 1, name: 'name1', surn: 'surn1' },
+				{ id: 2, name: 'name2', surn: 'surn2' },
+				{ id: 3, name: 'name3', surn: 'surn3' },
+			],
+		};
+	},
+	methods: {
+		change(id, name, surn) {
+			this.users = this.users.map((user) => {
+				if (user.id === id) {
+					user.name = name;
+					user.surn = surn;
+				}
+				return user;
+			});
+		},
+	},
 };
 </script>
--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>
-    {{ name }} {{ surn }}
-    <button @click="$emit('remove', id)">remove</button>
-  </div>
+	<div>
+		<template v-if="!isEdit">
+			{{ name }} {{ surn }}
+			<button @click="edit">edit</button>
+		</template>
+		<template v-else>
+			<input v-model="newName" />
+			<input v-model="newSurn" />
+			<button @click="save">save</button>
+		</template>
+	</div>
 </template>
 
 <script>
 export default {
-  props: {
-    id: Number,
-    name: String,
-    surn: String,
-  },
-  emits: ['remove'],
+	props: {
+		id: Number,
+		name: String,
+		surn: String,
+	},
+	emits: ['change'],
+	data() {
+		return {
+			isEdit: false,
+			newName: this.name,
+			newSurn: this.surn,
+		};
+	},
+	methods: {
+		edit() {
+			this.isEdit = true;
+		},
+		save() {
+			this.isEdit = false;
+			this.$emit('change', this.id, this.newName, this.newSurn);
+		},
+	},
 };
 </script>
