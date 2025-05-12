413ee23b75b69faeb1277b6ad155336105bb49d1
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:25:49 +0300

    task 67

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,5 @@
 <template>
-  <div>
-    <Employee @sendName="func" @sendEmployee="funcWithSalary" />
-  </div>
+	<button @click="$emit('customEvent', 'parameter1', 'parameter2')">
+		Нажми меня
+	</button>
 </template>
-
-<script>
-import Employee from './components/Employee.vue';
-
-export default {
-  components: {
-    Employee,
-  },
-  methods: {
-    // Метод для обработки имени работника
-    func(name) {
-      console.log(name);  // Выводим имя работника в консоль
-    },
-
-    // Метод для обработки имени и зарплаты работника
-    funcWithSalary(name, salary) {
-      console.log(name, salary);  // Выводим имя и зарплату работника в консоль
-    },
-  },
-};
-</script>
