1542cd999ba4253d497b501f385aa2d4254116c8
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:26:20 +0300

    task 8

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
 	<div>
-		<!-- Вывод значений объекта -->
-		<p>{{ obj.a }}</p>
-		<p>{{ obj.b }}</p>
-		<p>{{ obj.c }}</p>
-
-		<!-- Сумма элементов объекта -->
-		<p>Сумма: {{ obj.x + obj.y + obj.z }}</p>
+		<!-- Кнопка для вызова метода -->
+		<button @click="showDate">Показать текущую дату</button>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'App',
-	data() {
-		return {
-			obj: { x: 1, y: 2, z: 3 }
-		};
+	methods: {
+		showDate() {
+			// Получаем текущую дату и показываем её в alert
+			alert(new Date().toLocaleString());
+		}
 	}
 }
 </script>
