713ffade76cf0a49e4badca12b44dc7be6677675
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:39:00 +0300

    task 22

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,13 @@
 <template>
-	<div>
-		<!-- Абзац 1 -->
-		<button @click="toggle(1)">
-			{{ visible1 ? 'hide' : 'show' }}
-		</button>
-		<p v-if="visible1">text 1</p>
-
-		<!-- Абзац 2 -->
-		<button @click="toggle(2)">
-			{{ visible2 ? 'hide' : 'show' }}
-		</button>
-		<p v-if="visible2">text 2</p>
-
-		<!-- Абзац 3 -->
-		<button @click="toggle(3)">
-			{{ visible3 ? 'hide' : 'show' }}
-		</button>
-		<p v-if="visible3">text 3</p>
-	</div>
+	<p v-if="isAdmin">Admin</p>
+	<p v-else>User</p>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			visible1: false,
-			visible2: false,
-			visible3: false,
-		}
-	},
-	methods: {
-		toggle(number) {
-			this[`visible${number}`] = !this[`visible${number}`];
+			isAdmin: true, // или false
 		}
 	}
 }
