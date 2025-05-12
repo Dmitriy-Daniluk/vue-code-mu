9dac60ff8997e869aba80ac22fef7f9440822781
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:49:03 +0300

    task 32

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,5 @@
 <template>
-	<!-- Ссылка на страницы -->
-	<ul>
-		<template v-for="href in hrefs" :key="href.href">
-			<li><a :href="href.href">{{ href.text }}</a></li>
-		</template>
-	</ul>
-
-	<!-- Таблица с продуктами -->
-	<table>
-		<template v-for="product in products" :key="product.name">
-			<tr>
-				<td>{{ product.name }}</td>
-				<td>{{ product.price }}</td>
-				<td>{{ product.quantity }}</td>
-			</tr>
-		</template>
-	</table>
+	<p v-for="num in 30" :key="num">
+		{{ num }}
+	</p>
 </template>
-
-<script>
-export default {
-	data() {
-		return {
-			hrefs: [
-				{ href: '1.html', text: 'text1' },
-				{ href: '2.html', text: 'text2' },
-				{ href: '3.html', text: 'text3' },
-			],
-			products: [
-				{ name: 'product1', price: 100, quantity: 5 },
-				{ name: 'product2', price: 200, quantity: 4 },
-				{ name: 'product3', price: 300, quantity: 3 },
-			],
-		};
-	},
-};
-</script>
