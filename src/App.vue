e3b32827046a8586112bff632f03b972fd957471
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:27:17 +0300

    task 68

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <template>
-	<button @click="$emit('customEvent', 'parameter1', 'parameter2')">
-		Нажми меня
-	</button>
+  <div>
+    <Employee
+      v-for="user in users"
+      :key="user.id"
+      :id="user.id"
+      :name="user.name"
+      :surn="user.surn"
+      @remove="remove"
+    />
+  </div>
 </template>
+
+<script>
+import Employee from './components/Employee.vue';
+
+export default {
+  components: {
+    Employee,
+  },
+  data() {
+    return {
+      users: [
+        { id: 1, name: 'name1', surn: 'surn1' },
+        { id: 2, name: 'name2', surn: 'surn2' },
+        { id: 3, name: 'name3', surn: 'surn3' },
+      ],
+    };
+  },
+  methods: {
+    remove(id) {
+      this.users = this.users.filter((user) => user.id !== id);
+    },
+  },
+};
+</script>
--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
-    <button @click="sendEmployeeName">Send Employee Name</button>
-    <button @click="sendEmployeeDetails">Send Employee Details</button>
+    {{ name }} {{ surn }}
+    <button @click="$emit('remove', id)">remove</button>
   </div>
 </template>
 
 <script>
 export default {
-  emits: ['sendName', 'sendEmployee'],
-  methods: {
-    // Отправляем только имя работника
-    sendEmployeeName() {
-      this.$emit('sendName', 'John Doe');  // Передаем имя работника
-    },
-
-    // Отправляем имя и зарплату работника
-    sendEmployeeDetails() {
-      this.$emit('sendEmployee', 'John Doe', 50000);  // Передаем имя и зарплату работника
-    },
+  props: {
+    id: Number,
+    name: String,
+    surn: String,
   },
+  emits: ['remove'],
 };
 </script>
