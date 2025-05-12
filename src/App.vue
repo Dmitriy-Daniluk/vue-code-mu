9c5232bc492c51e27a7d5d9647c592d0f548ac6a
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:06:09 +0300

    task 54

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,21 @@
 <template>
-	<div>
-		<!-- Задание 1: Добавление в конец массива -->
-		<input v-model="newItem" />
-		<button @click="addItem">Add</button>
-
-		<ul>
-			<li v-for="(item, index) in items" :key="index">
-				{{ item }}
-			</li>
-		</ul>
-
-		<!-- Задание 2: Добавление в начало массива -->
-		<input v-model="newItem" />
-		<button @click="addItemToStart">Add to Start</button>
-
-		<ul>
-			<li v-for="(item, index) in items" :key="index">
-				{{ item }}
-			</li>
-		</ul>
-	</div>
+	<ul>
+		<li v-for="(item, index) in items" :key="index" @click="removeItem(index)">
+			{{ item }}
+		</li>
+	</ul>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			newItem: '',
 			items: ['a', 'b', 'c', 'd', 'e'],
 		};
 	},
 	methods: {
-		addItem() {
-			this.items.push(this.newItem);
-			this.newItem = ''; // Очистка инпута после добавления
-		},
-		addItemToStart() {
-			this.items.unshift(this.newItem);
-			this.newItem = ''; // Очистка инпута после добавления
+		removeItem(index) {
+			this.items.splice(index, 1);
 		}
 	}
 };
