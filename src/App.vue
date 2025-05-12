029877bbe7683e86c9395a28693c492b23a27a17
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:24:50 +0300

    task 66

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-	<div>
-		<Employee @action1="handleAction1" @action2="handleAction2" />
-	</div>
+  <div>
+    <Employee @sendName="func" @sendEmployee="funcWithSalary" />
+  </div>
 </template>
 
 <script>
 import Employee from './components/Employee.vue';
 
 export default {
-	components: {
-		Employee,
-	},
-	methods: {
-		handleAction1() {
-			alert('Action 1 triggered!');
-		},
-		handleAction2() {
-			alert('Action 2 triggered!');
-		},
-	},
+  components: {
+    Employee,
+  },
+  methods: {
+    // Метод для обработки имени работника
+    func(name) {
+      console.log(name);  // Выводим имя работника в консоль
+    },
+
+    // Метод для обработки имени и зарплаты работника
+    funcWithSalary(name, salary) {
+      console.log(name, salary);  // Выводим имя и зарплату работника в консоль
+    },
+  },
 };
 </script>
--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,20 +1,23 @@
 <template>
-	<div>
-		<button @click="triggerAction1">Action 1</button>
-		<button @click="triggerAction2">Action 2</button>
-	</div>
+  <div>
+    <button @click="sendEmployeeName">Send Employee Name</button>
+    <button @click="sendEmployeeDetails">Send Employee Details</button>
+  </div>
 </template>
 
 <script>
 export default {
-	emits: ['action1', 'action2'],
-	methods: {
-		triggerAction1() {
-			this.$emit('action1');
-		},
-		triggerAction2() {
-			this.$emit('action2');
-		},
-	},
+  emits: ['sendName', 'sendEmployee'],
+  methods: {
+    // Отправляем только имя работника
+    sendEmployeeName() {
+      this.$emit('sendName', 'John Doe');  // Передаем имя работника
+    },
+
+    // Отправляем имя и зарплату работника
+    sendEmployeeDetails() {
+      this.$emit('sendEmployee', 'John Doe', 50000);  // Передаем имя и зарплату работника
+    },
+  },
 };
 </script>
