c962130a4b8f25cd993b5f2b989286efb56b7554
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:58:14 +0300

    task 43

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-	<p :style="{ color: 'green', backgroundColor: 'yellow' }">
-		text
-	</p>
-	<p :style="{ fontWeight: 'bold', fontStyle: 'italic' }">
-		text
-	</p>
+	<div>
+		<!-- Задача 1: Текст из инпута сразу выводится в абзаце -->
+		<input v-model="message">
+		<p>{{ message }}</p>
+
+		<!-- Задача 2: Текст выводится в верхнем регистре -->
+		<input v-model="message">
+		<p>{{ message.toUpperCase() }}</p>
+
+		<!-- Задача 3: Квадрат введенного числа -->
+		<input v-model.number="number" type="number">
+		<p>{{ number * number }}</p>
+	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			message: '',
+			number: 0,
+		};
+	},
+};
+</script>
