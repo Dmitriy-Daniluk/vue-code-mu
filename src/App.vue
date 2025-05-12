28be6b91944285bb23d3f3b234ed7dda7d97a593
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:21:20 +0300

    task 65

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div>
-    <Employee @action1="action1" @action2="action2" />
-  </div>
+	<div>
+		<Employee @action1="handleAction1" @action2="handleAction2" />
+	</div>
 </template>
 
 <script>
 import Employee from './components/Employee.vue';
 
 export default {
-  components: {
-    Employee,
-  },
-  methods: {
-    action1() {
-      alert('Action 1 triggered!');
-    },
-    action2() {
-      alert('Action 2 triggered!');
-    },
-  },
+	components: {
+		Employee,
+	},
+	methods: {
+		handleAction1() {
+			alert('Action 1 triggered!');
+		},
+		handleAction2() {
+			alert('Action 2 triggered!');
+		},
+	},
 };
 </script>
--- src/components/Employee.vue
+++ src/components/Employee.vue
@@ -1,18 +1,18 @@
 <template>
 	<div>
-		<button @click="handleAction1">Action 1</button>
-		<button @click="handleAction2">Action 2</button>
+		<button @click="triggerAction1">Action 1</button>
+		<button @click="triggerAction2">Action 2</button>
 	</div>
 </template>
 
 <script>
 export default {
-	props: ['action1', 'action2'],
+	emits: ['action1', 'action2'],
 	methods: {
-		handleAction1() {
+		triggerAction1() {
 			this.$emit('action1');
 		},
-		handleAction2() {
+		triggerAction2() {
 			this.$emit('action2');
 		},
 	},
