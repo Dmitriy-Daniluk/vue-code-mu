871522b39632d55a3e312b9895f625056aaa3d79
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:40:31 +0300

    task 24

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
-	<p v-if="day === 1">Понедельник</p>
-	<p v-if="day === 2">Вторник</p>
-	<p v-if="day === 3">Среда</p>
-	<p v-if="day === 4">Четверг</p>
-	<p v-if="day === 5">Пятница</p>
-	<p v-if="day === 6">Суббота</p>
-	<p v-if="day === 7">Воскресенье</p>
+	<p v-if="age < 18">Подросток</p>
+	<p v-else-if="age >= 19 && age <= 25">Молодой человек</p>
+	<p v-else>Мужчина</p>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			day: 3,
+			age: 25,
 		}
 	}
 }
