c05999e1b6c928d295186ca30cb001002d0b230b
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:06:52 +0300

    task 55

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
-	<ul>
-		<li v-for="(item, index) in items" :key="index" @click="removeItem(index)">
-			{{ item }}
-		</li>
-	</ul>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Salary</th>
+        <th>Age</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.name }}</td>
+        <td>{{ user.salary }}</td>
+        <td>{{ user.age }}</td>
+        <td><a href="#" @click="removeItem(user.id)">Remove</a></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			items: ['a', 'b', 'c', 'd', 'e'],
-		};
-	},
-	methods: {
-		removeItem(index) {
-			this.items.splice(index, 1);
-		}
-	}
+  data() {
+    return {
+      users: [
+        { id: 1, name: 'name1', salary: 100, age: 30 },
+        { id: 2, name: 'name2', salary: 200, age: 40 },
+        { id: 3, name: 'name3', salary: 300, age: 50 },
+      ],
+    };
+  },
+  methods: {
+    removeItem(id) {
+      this.users = this.users.filter(user => user.id !== id);
+    },
+  },
 };
 </script>
