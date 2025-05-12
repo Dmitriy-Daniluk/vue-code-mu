4a723393190b303e324099bdde65c42d91b5c74a
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:48:25 +0300

    task 31

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
-	<div>
-		<template v-for="elem in items" :key="elem">
-			<p>{{ elem }}</p>
-			<p class="divider"></p>
-		</template>
-	</div>
-
+	<!-- Ссылка на страницы -->
 	<ul>
-		<template v-for="elem in items" :key="elem">
-			<li>{{ elem }}</li>
-			<li class="divider"></li>
+		<template v-for="href in hrefs" :key="href.href">
+			<li><a :href="href.href">{{ href.text }}</a></li>
 		</template>
 	</ul>
+
+	<!-- Таблица с продуктами -->
+	<table>
+		<template v-for="product in products" :key="product.name">
+			<tr>
+				<td>{{ product.name }}</td>
+				<td>{{ product.price }}</td>
+				<td>{{ product.quantity }}</td>
+			</tr>
+		</template>
+	</table>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			items: [1, 2, 3],
+			hrefs: [
+				{ href: '1.html', text: 'text1' },
+				{ href: '2.html', text: 'text2' },
+				{ href: '3.html', text: 'text3' },
+			],
+			products: [
+				{ name: 'product1', price: 100, quantity: 5 },
+				{ name: 'product2', price: 200, quantity: 4 },
+				{ name: 'product3', price: 300, quantity: 3 },
+			],
 		};
 	},
 };
