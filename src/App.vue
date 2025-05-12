075d7337836dce3653f1f9d50a64efd970d45fe8
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:29:41 +0300

    task 12

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<div>
-		<button @click="show">Показать текст</button>
+		<!-- Кнопка для показа текущего дня недели -->
+		<button @click="showCurrentDay">Показать текущий день недели</button>
+
+		<!-- Кнопка для показа дня недели по текущему числу -->
+		<button @click="showDayFromNumber">Показать день недели по числу</button>
 	</div>
 </template>
 
@@ -9,13 +13,27 @@ export default {
 	name: 'App',
 	data() {
 		return {
-			text: 'str'
+			// Получаем текущий день недели (0 - воскресенье, 6 - суббота)
+			dayNumber: new Date().getDay(),
+			// Можно задать любое число для теста (например, 2 - вторник)
+			testNumber: 2
 		};
 	},
 	methods: {
-		show() {
-			// Выводим значение свойства text
-			alert(this.text);
+		// Вспомогательный метод для получения дня недели по числу
+		getDayByNumber(number) {
+			const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+			return days[number];
+		},
+		// Основной метод для вывода текущего дня недели
+		showCurrentDay() {
+			const currentDay = this.getDayByNumber(this.dayNumber);
+			alert(`Сегодня: ${currentDay}`);
+		},
+		// Основной метод для вывода дня недели по числу
+		showDayFromNumber() {
+			const day = this.getDayByNumber(this.testNumber);
+			alert(`День недели для числа ${this.testNumber}: ${day}`);
 		}
 	}
 }
