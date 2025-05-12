efaad0dbf26bbb9021e8c26a5a3ba7eab9ac815c
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:56:56 +0300

    task 41

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
 	<div>
-		<!-- Тег с инлайн объектом с классами -->
-		<p :class="{ active: true, valid: false }">text</p>
+		<!-- Элемент с динамически добавляемыми классами -->
+		<p :class="{ valid: isValid, disabled: isDisabled }">
+			text
+		</p>
 	</div>
 </template>
 
 <script>
 export default {
 	data() {
-		return {};
+		return {
+			isValid: true,
+			isDisabled: true,
+		};
 	}
 };
 </script>
 
 <style>
 /* Стили для классов */
-.active {
+.valid {
 	color: green;
 }
 
-.valid {
-	color: red;
+.disabled {
+	color: gray;
+	text-decoration: line-through;
 }
 </style>
