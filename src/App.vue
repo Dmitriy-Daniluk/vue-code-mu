fccbce6f074dbd7c025586da23d44fd63fa2a933
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:44:45 +0300

    task 27

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,22 @@
 <template>
-	<strong>№1. Дан абзац и кнопка. Сделайте так, чтобы абзац тогглился по нажатию на кнопку.</strong>
-	<button @click="toggleVisibility">Тогглить абзац</button>
-	<p v-show="isVisible">Абзац для тогглинга</p>
-	<ul>
-  <li><strong>№2. Отличия директив v-show и v-if:</strong>
-    <ul>
-      <li><strong>v-if</strong> выполняет условную отрисовку элемента. Если условие ложно, элемент не отрисовывается в DOM, и события не активны.</li>
-      <li><strong>v-show</strong> всегда отрисовывает элемент, скрывая его с помощью CSS (через свойство `display: none`). События остаются привязанными, но элемент не виден.</li>
-    </ul>
-  </li>
-  
-  <li><strong>№3. Когда лучше использовать директиву v-show, а когда v-if:</strong>
-    <ul>
-      <li><strong>v-show</strong> лучше использовать, когда нужно часто переключать видимость элемента, так как переключение состояния быстрее (изменяется только CSS).</li>
-      <li><strong>v-if</strong> лучше использовать, когда элемент должен быть создан и уничтожен в зависимости от условия, особенно если условие меняется редко.</li>
-    </ul>
-  </li>
-  
-  <li><strong>№4. Ограничения директивы v-show:</strong>
-    <ul>
-      <li>Директиву <strong>v-show</strong> нельзя использовать на элементе <code>&lt;template&gt;</code>.</li>
-      <li>Она не работает с <strong>v-else</strong>, то есть нельзя использовать их вместе для одного элемента.</li>
-    </ul>
-  </li>
-</ul>
+	<!-- №1 -->
+	<div v-for="item in items" :key="item">{{ item }}</div>
 
+	<!-- №2 -->
+	<div v-for="item in items" :key="item">{{ item * item }}</div>
+
+	<!-- №3 -->
+	<ul>
+		<li v-for="item in items" :key="item">{{ item }}</li>
+	</ul>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			isVisible: true
-		}
+			items: [1, 2, 3, 4, 5],
+		};
 	},
-	methods: {
-		toggleVisibility() {
-			this.isVisible = !this.isVisible;
-		}
-	}
-}
+};
 </script>
