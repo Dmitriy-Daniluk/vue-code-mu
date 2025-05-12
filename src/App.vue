c328883356d33413be5a48cc559091b82b8df1ba
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:28:02 +0300

    task 11

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
 	<div>
-		<!-- Кнопка для вывода текущей даты по клику -->
-		<button @click="showDate">Показать текущую дату (клик)</button>
-
-		<!-- Кнопка для вывода текущей даты по наведению мыши -->
-		<button @mouseover="showDate">Показать текущую дату (наведение)</button>
+		<button @click="show">Показать текст</button>
 	</div>
 </template>
 
 <script>
 export default {
 	name: 'App',
+	data() {
+		return {
+			text: 'str'
+		};
+	},
 	methods: {
-		showDate() {
-			// Получаем текущую дату и показываем её в alert
-			alert(new Date().toLocaleString());
+		show() {
+			// Выводим значение свойства text
+			alert(this.text);
 		}
 	}
 }
