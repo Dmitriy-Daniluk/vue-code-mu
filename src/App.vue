d1df6d35731e12380b44c0d337aa94152744f26e
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 02:09:14 +0300

    task 58

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,22 @@
 <template>
-	<table>
-		<thead>
-			<tr>
-				<th>Name</th>
-				<th>Surname</th>
-				<th>Actions</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr v-for="user in users" :key="user.id">
-				<td>
-					<template v-if="!user.isEdit">
-						{{ user.name }}
-					</template>
-					<template v-else>
-						<input v-model="user.name" />
-					</template>
-				</td>
-				<td>
-					<template v-if="!user.isEdit">
-						{{ user.surn }}
-					</template>
-					<template v-else>
-						<input v-model="user.surn" />
-					</template>
-				</td>
-				<td>
-					<template v-if="!user.isEdit">
-						<button @click="edit(user)">Edit</button>
-					</template>
-					<template v-else>
-						<button @click="save(user)">Save</button>
-					</template>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+  <div id="app">
+    <Employee />
+  </div>
 </template>
 
 <script>
+import Employee from './components/Employee.vue';
+
 export default {
-	data() {
-		return {
-			users: [
-				{ id: 1, name: 'name1', surn: 'surn1', isEdit: false },
-				{ id: 2, name: 'name2', surn: 'surn2', isEdit: false },
-				{ id: 3, name: 'name3', surn: 'surn3', isEdit: false },
-			],
-		};
-	},
-	methods: {
-		edit(user) {
-			user.isEdit = true;
-		},
-		save(user) {
-			user.isEdit = false;
-		},
-	},
+  name: 'App',
+  components: {
+    Employee
+  }
 };
 </script>
+
+<style>
+#app {
+  text-align: center;
+}
+</style>
---
+++ src/components/Employee.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h2>Employee: {{ name }}</h2>
+		<p>Position: {{ position }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name: 'john',
+			position: 'Developer'
+		};
+	}
+};
+</script>
+
+<style scoped>
+h2 {
+	color: blue;
+}
+</style>
