c873623a8ccc8f195f4366bc98270758511f1f00
Dmitriy-Daniluk <danilyuk.d.i.23@gmail.com>
2025-05-13 01:35:30 +0300

    task 17

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
-	<!-- Ссылка с обработчиком события и модификатором prevent -->
-	<a href="https://yandex.ru" @click.prevent="handleLinkClick">Click me</a>
+	<p v-if="visible">Первый абзац (visible = true)</p>
 
-	<!-- Кнопка с обработчиком события, срабатывающего только один раз -->
-	<button @click.once="handleButtonClick">Click me once</button>
+	<p v-if="!visible">Второй абзац (visible = false)</p>
 </template>
 
 <script>
 export default {
-	methods: {
-		// Обработчик клика по ссылке
-		handleLinkClick() {
-			alert('Ссылка была нажата, но переход не произошел!');
-		},
-
-		// Обработчик клика по кнопке
-		handleButtonClick() {
-			alert('Вы кликнули по кнопке первый раз!');
-		}
+	data() {
+		return {
+			visible: true,
+		};
 	}
 }
 </script>
